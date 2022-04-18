706c625791b463bc3bfa34c9d6646e35e6aa6ed4
Norel <norelp123@gmail.com>
2022-04-18 11:12:36 -0700

    init

---
+++ providers.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  region = "eu-central-1"
+  access_key = "AKIA262NWF7U3LOFJTVO" // bad idea <-----------------------------------------
+  secret_key = "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" // bad idea  <--------------------
+}
